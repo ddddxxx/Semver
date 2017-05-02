ef78f22445b4af0b6d416da9391006b226c618fa
Xander Deng <dengxiang2010@gmail.com>
2017-05-02 10:31:29 +0800

    Initial Commit

---
+++ Semver/Semver.h
@@ -0,0 +1,19 @@
+//
+//  Semver.h
+//  Semver
+//
+//  Created by 邓翔 on 2017/5/2.
+//  Copyright © 2017年 ddddxxx. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Semver.
+FOUNDATION_EXPORT double SemverVersionNumber;
+
+//! Project version string for Semver.
+FOUNDATION_EXPORT const unsigned char SemverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Semver/PublicHeader.h>
+
+
