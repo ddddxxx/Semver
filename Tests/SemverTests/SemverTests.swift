38a391123d52fcc906af4d521b250294f950c5a3
Xander Deng <dengxiang2010@gmail.com>
2017-05-03 11:53:32 +0800

    update: test case

--- Tests/SemverTests/SemverTests.swift
+++ Tests/SemverTests/SemverTests.swift
@@ -11,26 +11,129 @@ import XCTest
 
 class SemverTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testParserErrors() {
+        for str in badVersionStrings {
+            XCTAssertThrowsError(try Semver(str), "'\(str)' should throws")
         }
     }
     
+    func testVersionEquality() {
+        for (left, right) in notEqualVersionPairs {
+            XCTAssertNotEqual(try! Semver(left), try! Semver(right), "'\(left)' and '\(right)' should not be equal")
+        }
+        
+        for (left, right) in equalVersionPairs {
+            XCTAssertEqual(try! Semver(left), try! Semver(right), "'\(left)' and '\(right)' should be equal")
+        }
+    }
+    
+    func testVersionComparison() {
+        let sortedVersions = versionStringsToBeSort.map {try! Semver($0)}.sorted()
+        let preSortedVersions = preSortedVersionStrings.map {try! Semver($0)}
+        
+        XCTAssertEqual(sortedVersions, preSortedVersions, "Versions not sorted properly!")
+    }
 }
+
+let badVersionStrings = [
+    // no enough elements
+    "", "2", "2.1",
+    "5.2-alpha.0", "5-alpha.0",
+    // invalid character
+    "*.1", "5.1.^", "5.#.2",
+    "2.2.1-alpha.*", "2.2.1-3.*.boom", "12.256.3-3.*.boom.45",
+    "2.2.1+h*23", "2.2.1-alpha.0+alkj&^",
+    // too many elements
+    "5.0.0.0",
+    // .- sequence
+    "5.0.0.-alpha.0", "5.0.-alpha.0", "5.-alpha.0",
+    // empty components
+    "5.0..", ".3.1",
+    "5.0.0-alpha..0",
+    "2.3.5+h..3",
+    // no pre-release info
+    "2.2.1-+hello",
+]
+
+let notEqualVersionPairs: [(String, String)] = [
+    ("3.0.0",           "3.0.0-alpha.0"),
+    ("3.0.0",           "3.0.1"),
+    ("3.0.0",           "3.1.0"),
+    ("4.0.0",           "3.0.0"),
+    ("3.0.0-alpha.0",   "3.0.0-alpha.5"),
+    ("3.0.0-alpha.0",   "3.0.0-beta.0"),
+    ("3.0.0-alpha.0",   "3.0.0-boo"),
+    ("3.0.0",           "3.1.1"),
+    ("3.0.0-alpha.0",   "3.0.1-alpha.1")
+]
+
+let equalVersionPairs: [(String, String)] = [
+    ("3.0.0",           "3.0.0"),
+    ("3.0.1",           "3.0.1"),
+    ("3.1.0",           "3.1.0"),
+    ("3.0.0-alpha.0",   "3.0.0-alpha.0"),
+    ("3.0.0-beta.0",    "3.0.0-beta.0"),
+    ("3.0.0-boo",       "3.0.0-boo"),
+    ("3.0.0",           "3.0.0+metadata"),
+    ("3.0.1",           "3.0.1+metadata"),
+    ("3.1.0",           "3.1.0+metadata"),
+    ("3.0.0-alpha.0",   "3.0.0-alpha.0+metadata"),
+    ("3.0.0-beta.0",    "3.0.0-beta.0+metadata"),
+    ("3.0.0-boo",       "3.0.0-boo+metadata"),
+]
+
+let versionStringsToBeSort = [
+    "0.1.0-rc.1",
+    "0.0.1-alpha.0",
+    "0.0.1",
+    "0.0.2-alpha.0",
+    "0.0.2-alpha.0.1",
+    "0.0.2",
+    "0.0.3-aaa.11",
+    "0.0.3-aaa.2",
+    "0.0.3-aaa",
+    "0.0.3-alpha.1",
+    "0.1.0-beta.2",
+    "0.1.0-beta.3",
+    "0.1.0",
+    "1.0.1",
+    "1.0.0-alpha.0",
+    "1.0.0",
+    "0.0.2-alpha",
+    "1.1.0",
+    "0.1.0-alpha.3",
+    "1.2.0",
+    "2.0.0-alpha.0",
+    "2.0.0-1",
+    "1.0.0-1",
+    "1.0.0-3",
+    "1.0.0-11",
+]
+
+let preSortedVersionStrings = [
+    "0.0.1-alpha.0",
+    "0.0.1",
+    "0.0.2-alpha",
+    "0.0.2-alpha.0",
+    "0.0.2-alpha.0.1",
+    "0.0.2",
+    "0.0.3-aaa",
+    "0.0.3-aaa.2",
+    "0.0.3-aaa.11",
+    "0.0.3-alpha.1",
+    "0.1.0-alpha.3",
+    "0.1.0-beta.2",
+    "0.1.0-beta.3",
+    "0.1.0-rc.1",
+    "0.1.0",
+    "1.0.0-1",
+    "1.0.0-3",
+    "1.0.0-11",
+    "1.0.0-alpha.0",
+    "1.0.0",
+    "1.0.1",
+    "1.1.0",
+    "1.2.0",
+    "2.0.0-1",
+    "2.0.0-alpha.0"
+]
